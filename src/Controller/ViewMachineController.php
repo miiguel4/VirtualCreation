5d52f6ef81755bd5e702643e8a69e53497a25d40
miiguel4 <mruirub878@g.educaand.es>
2022-06-08 02:16:46 -0700

    Interfaz ViewMaquinas

--- src/Controller/ViewMachineController.php
+++ src/Controller/ViewMachineController.php
@@ -59,18 +59,22 @@ class ViewMachineController extends AbstractController
 
             $nombremaquina = shell_exec("govc find vm -name *".$nombreacortado."");
             $maquinas = explode("\n", $nombremaquina);
-           
-            foreach ($maquinas as $key => $value) {
+            
+            if (isset($_POST['checkbox'])) {
+                foreach ($maquinas as $key => $value) {
               
-                if ($value == $_POST['nombre']) {
-                    $console =  shell_exec("govc vm.console /ha-datacenter/".$value);
-                    $moid= $nombreacortado = substr(  $console, -3);
-                    $number = $moid;
-                    $url = "vmrc://global@192.168.1.38/?moid=$number";
-                    $url2 = trim($url);
-                    echo '<script>window.open ("'.$url2.'")</script>';
+                    if ($value == $_POST['checkbox']) {
+                        $console =  shell_exec("govc vm.console /ha-datacenter/".$value);
+                        $moid= $nombreacortado = substr(  $console, -3);
+                        $number = $moid;
+                        $url = "vmrc://global@192.168.1.38/?moid=$number";
+                        $url2 = trim($url);
+                        echo '<script>window.open ("'.$url2.'")</script>';
+                    }
                 }
             }
+           
+            
         }
 
         if (isset($_POST['apagar'])) {
@@ -87,13 +91,15 @@ class ViewMachineController extends AbstractController
             $nombremaquina = shell_exec("govc find vm -name *".$nombreacortado."");
             $maquinas = explode("\n", $nombremaquina);
           
-
-            foreach ($maquinas as $key => $value) {
+            if (isset($_POST['checkbox'])) {
+                foreach ($maquinas as $key => $value) {
               
-                if ($value ==  $_POST['nombre']) {
-                    shell_exec("govc vm.power -off -force /ha-datacenter/$value");
+                    if ($value ==  $_POST['checkbox']) {
+                        shell_exec("govc vm.power -off -force /ha-datacenter/$value");
+                    }
                 }
             }
+            
         }
 
         if (isset($_POST['encender'])) {
@@ -110,13 +116,15 @@ class ViewMachineController extends AbstractController
             $nombremaquina = shell_exec("govc find vm -name *".$nombreacortado."");
             $maquinas = explode("\n", $nombremaquina);
           
-
-            foreach ($maquinas as $key => $value) {
+            if (isset($_POST['checkbox'])) {
+                foreach ($maquinas as $key => $value) {
               
-                if ($value ==  $_POST['nombre']) {
-                    shell_exec("govc vm.power -on /ha-datacenter/$value");
+                    if ($value ==  $_POST['checkbox']) {
+                        shell_exec("govc vm.power -on /ha-datacenter/$value");
+                    }
                 }
             }
+            
         }
 
         if (isset($_POST['descargar'])) {
@@ -154,22 +162,49 @@ class ViewMachineController extends AbstractController
             $nombremaquina = shell_exec("govc find vm -name *".$nombreacortado."");
             $maquinas = explode("\n", $nombremaquina);
           
-
-            foreach ($maquinas as $key => $value) {
+            if (isset($_POST['checkbox'])) {
+                foreach ($maquinas as $key => $value) {
               
-                if ($value ==  $_POST['nombre']) {
-                    $vm = substr($value, 3);
-
-                    $em = $this->getDoctrine()->getManager();
-                    $query = $em->createQuery(
-                        'DELETE App:Parametros n 
-                        WHERE n.Nombre = :nombre'
-                    )->setParameter("nombre", $vm);
-                    $query->execute();
-                    
-                    shell_exec(" govc vm.destroy /ha-datacenter/$value");
+                    if ($value ==  $_POST['checkbox']) {
+                        $vm = substr($value, 3);
+    
+                        $em = $this->getDoctrine()->getManager();
+                        $query = $em->createQuery(
+                            'DELETE App:Parametros n 
+                            WHERE n.Nombre = :nombre'
+                        )->setParameter("nombre", $vm);
+                        $query->execute();
+                        
+                        shell_exec(" govc vm.destroy /ha-datacenter/$value");
+                    }
                 }
             }
+            
+        }
+
+        if (isset($_POST['suspender'])) {
+            $email = $this->getUser()->getEmail();
+            $posicion = strpos($email, '@');
+            $nombreUser = substr($email, 0, $posicion);
+            $nombreacortado = substr($nombreUser, -6);
+        
+            putenv("GOVC_INSECURE=true");
+            putenv("GOVC_URL=https://root:Tt.676559546@192.168.1.38/sdk");
+            putenv("GOVC_DATASTORE=datastore1");
+            putenv("GOVC_NETWORK=VM Network");
+
+            $nombremaquina = shell_exec("govc find vm -name *".$nombreacortado."");
+            $maquinas = explode("\n", $nombremaquina);
+          
+            if (isset($_POST['checkbox'])) {
+
+                 foreach ($maquinas as $key => $value) {
+                    if ($value ==  $_POST['checkbox']) {
+                        shell_exec("govc vm.power -suspend=true /ha-datacenter/$value");
+                    }
+                 }
+            }
+           
         }
     }
 }
