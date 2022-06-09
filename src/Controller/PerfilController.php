aa63503c5a5255b759b675310f19061342fc2199
miiguel4 <mruirub878@g.educaand.es>
2022-06-09 11:22:32 -0700

    Perfil

--- src/Controller/PerfilController.php
+++ src/Controller/PerfilController.php
@@ -13,8 +13,28 @@ class PerfilController extends AbstractController
      */
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $email = $this->getUser()->getEmail();
+        $posicion = strpos($email, '@');
+        $nombreUser = substr($email, 0, $posicion);
+        $nombreacortado = substr($nombreUser, -6);
+
+        $id = $this->getUser()->getId();
+
+            putenv("GOVC_INSECURE=true");
+            putenv("GOVC_URL=https://root:Tt.676559546@192.168.1.38/sdk");
+            putenv("GOVC_DATASTORE=datastore1");
+            putenv("GOVC_NETWORK=VM Network");
+
+            $nombremaquina = shell_exec("govc find vm -name *".$id."");
+            $maquinas = explode("\n", $nombremaquina);
+            $numero = count($maquinas)-1;
+            
+
         return $this->render('perfil/index.html.twig', [
             'controller_name' => 'PerfilController',
+            'nombre'          => $nombreacortado,
+            'numeroMaquina'   => $numero
         ]);
     }
 }
